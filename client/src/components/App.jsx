aef6683098f306d5a7387cd3a1e7b4244d94c968
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-04-09 16:44:24 -0700

    Added in some components and proptyes to handle data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Container, Jumbotron, Button } from 'reactstrap';
+import PropTypes from 'prop-types';
+import NavBar from './NavBar';
+import CardsList from './CardsList';
 
-const App = () => (
+const App = ({ data }) => (
   <div>
-    Hello from Home Page
+    <Container fluid>
+      <NavBar />
+      <Jumbotron>
+        <h1 className="display-3">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-2" />
+        <p>This is some text</p>
+        <p className="lead">
+          <Button color="primary">Learn More</Button>
+        </p>
+      </Jumbotron>
+      <CardsList data={data} />
+    </Container>
   </div>
 );
 
 export default App;
+
+Jumbotron.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  fluid: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+Container.propTypes = {
+  fluid: PropTypes.bool,
+};
+
+App.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
