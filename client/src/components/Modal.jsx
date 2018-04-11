e2ca09e19b7712e3bcfd078497ec006a10937e3f
Airyque Ervin <airyqueervin@Airyque-Ervins-MacBook-Air.local>
2018-04-11 14:44:44 -0700

    Added carousel with data passed in.

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,42 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import Carousel from './Carousel';
 
-class ModalCarousel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-    };
-  }
-
-  render() {
-    const { modal, toggle } = this.props
-    return (
-      <div>
-        <Modal isOpen={modal} toggle={toggle} className={this.props.className}>
-          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
-}
+const ModalCarousel = ({
+  modal, toggle, img, title, url,
+}) => (
+  <div>
+    <Modal isOpen={modal} toggle={toggle} style={{ maxWidth: '75%' }}>
+      <ModalHeader toggle={toggle}>{title}</ModalHeader>
+      <ModalBody>
+        <Carousel items={img} />
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          color="primary"
+          onClick={() => {
+          window.open(url);
+          toggle();
+        }}
+        >Vist The App
+        </Button>{' '}
+        <Button color="secondary" onClick={toggle}>Cancel</Button>
+      </ModalFooter>
+    </Modal>
+  </div>
+);
 
 export default ModalCarousel;
+
+ModalCarousel.propTypes = {
+  toggle: PropTypes.func.isRequired,
+  modal: PropTypes.bool.isRequired,
+  img: PropTypes.arrayOf(PropTypes.string).isRequired,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
 
 Modal.propTypes = {
   // boolean to control the state of the popover
